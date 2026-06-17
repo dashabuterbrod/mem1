f97b0649e4a56dff2b7ee36ec72d4b421c98d93f
arestova-da <i@arestova-1.ru>
2026-06-17 19:05:06 +0700

    генерат1069ор

--- mem1/MainPage.xaml.cs
+++ mem1/MainPage.xaml.cs
@@ -1,8 +1,25 @@
-﻿namespace mem1;
+﻿using System.IO;
+
+namespace mem1;
 
 public partial class MainPage : ContentPage
 {
     int count = 0;
+    Random random = new Random();
+
+    // Переменная для хранения текущей картинки
+    string currentMeme = "memea.gif";
+
+    List<string> memeList = new List<string>
+    {
+        "one.gif", "two.webp", "three.gif", "foy.webp", "five.webp",
+        "six.gif", "seven.webp", "eight.gif", "nine.webp", "ten.webp",
+        "eleven.gif", "twenteen.webp", "thirteen.webp", "foyteen.webp", "fiveteen.webp",
+        "memeu.webp", "memey.gif", "memet.gif", "memex.webp", "memer.webp",
+        "memeq.gif", "memep.webp", "memeo.webp", "memen.webp", "memem.webp",
+        "memel.gif", "memek.webp", "memej.webp", "memei.webp", "memeh.webp",
+        "memev.webp", "memee.gif", "memed.webp", "memec.webp", "memeb.gif", "memea.gif"
+    };
 
     public MainPage()
     {
@@ -13,5 +30,34 @@ public partial class MainPage : ContentPage
     {
         count++;
         CountLabel.Text = $"Сгенерировано мемов: {count}";
+
+        int randomIndex = random.Next(memeList.Count);
+        currentMeme = memeList[randomIndex];
+        MemImage.Source = currentMeme;
+    }
+
+    private void OnFavoriteClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Уведомление", "Добавлено в избранное", "OK");
+    }
+
+    // Логика скачивания для Windows
+    private async void OnDownloadClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string targetPath = Path.Combine(docPath, currentMeme);
+
+            using var stream = await FileSystem.OpenAppPackageFileAsync(currentMeme);
+            using var fileStream = File.Create(targetPath);
+            await stream.CopyToAsync(fileStream);
+
+            await DisplayAlert("Успешно", $"Мем сохранен в папку Документы", "ОК");
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ошибка", $"Не удалось сохранить: {ex.Message}", "ОК");
+        }
     }
 }
